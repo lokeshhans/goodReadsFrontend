d3a6071fc2388333baf5ca88dad7d369a827bc25
Lokesh <hanswork@gmail.com>
2025-10-15 12:38:49 +0530

    added daisy ui for easy development

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1 className='bg-red-50'>app</h1>
+      <h1 className="heading">app</h1>
+      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
