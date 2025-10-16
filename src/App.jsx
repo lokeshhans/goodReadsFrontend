88c37c7ff2b2b5f28d9a37d6bd8ccb5ae736f316
Lokesh <hanswork@gmail.com>
2025-10-16 07:30:10 +0530

    Added absolute import config using vite.config and jsconfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
 
+import Navbar from "Components/Navbar/Navbar";
+import Home from "Pages/Home/Home";
+
 function App() {
   return (
     <>
+    <Navbar />
+    <Home />
       <h1 className="heading">app</h1>
       <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
         Button
--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,19 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      src: '/src',
+      Assets: '/src/Assets',
+      Components: '/src/Components',
+      Configs: '/src/Configs',
+      Helpers: '/src/Helpers',
+      Hooks: '/src/Hooks',
+      Layouts: '/src/Layouts',
+      Pages: '/src/Pages',
+      Redux: '/src/Redux',
+      Routes: '/src/Routes',
+      Utils: '/src/Utils',
+    }
+  }
 });
