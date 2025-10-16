934a6105404447f757552752b954b095dfe96acb
Lokesh <hanswork@gmail.com>
2025-10-16 10:39:25 +0530

    added home Page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import logo from "Assets/Images/logo.ico";
+import React from "react";
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="flex items-center justify-center flex-col h-screen gap-20 ">
+      <div className="logo-img flex flex-col items-center justify-center h-[50vh]">
+        <img className="h-[100%]" src={logo} alt="" />
+      </div>
+      <div className="flex">
+        <div className="flex items-center justify-center flex-col text-center text-5xl mt-10 rounded-lg  font-semibold w-2/3 ">
+          <h1 className="text-white w-2/3 ">
+            BookShelf
+            <br />
+            <span className="text-warning">Your Personal Library And Social Network for Bookworms</span>
+          </h1>
+        </div>
+        <div className="flex items-center justify-center  text-center ">
+          <button class="btn btn-primary p-6 text-2xl text-white ">register</button>
+          <button class="btn btn-warning p-6 text-2xl mx-3 text-white ">login</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
